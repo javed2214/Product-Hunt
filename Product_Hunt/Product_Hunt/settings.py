7c158b8d43918608f293b23f1c6af73f022330f9
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2019-07-19 00:58:19 +0530

    Update settings.py

--- Product_Hunt/Product_Hunt/settings.py
+++ Product_Hunt/Product_Hunt/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nchsqz_txyew6@1!bu_t1evwx+2re9&nw63_d@be6h^q47e8c_'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
